452cbb82f98008d3f80a3c32af716cbe2c8870a2
Julian Cheal <jcheal@redhat.com>
2015-12-18 00:58:28 +0000

    Added execute file permissions to scripts

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -478,10 +478,10 @@ defaults write com.twitter.twitter-mac HideInBackground -bool true
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-	"Dock" "Finder" "Google Chrome" "Mail" "Messages" \
-	"Opera" "Safari" "SystemUIServer" "Terminal" \
-	"Twitter" "iCal"; do
-	killall "${app}" &> /dev/null
-done
-echo "Done. Note that some of these changes require a logout/restart to take effect."
+# for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+# 	"Dock" "Finder" "Google Chrome" "Mail" "Messages" \
+# 	"Opera" "Safari" "SystemUIServer" "Terminal" \
+# 	"Twitter" "iCal"; do
+# 	killall "${app}" &> /dev/null
+# done
+# echo "Done. Note that some of these changes require a logout/restart to take effect."
